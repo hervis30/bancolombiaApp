b9413acae25d33414d9da7a21f5f43eb0d3267bb
Hervis <hervisjonat@gmail.com>
2022-03-28 17:43:11 -0500

    guardando trabajo con error

--- app/src/main/java/com/example/bancolombiaapp/MainActivity.java
+++ app/src/main/java/com/example/bancolombiaapp/MainActivity.java
@@ -7,14 +7,39 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Html;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.bancolombiaapp.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mainBinding;
+    private String username;
+    //private String image;
+    //private String status;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +48,24 @@ public class MainActivity extends AppCompatActivity {
         View view = mainBinding.getRoot();
         setContentView(view);
 
-        mainBinding.txtNoCliente.setPaintFlags(mainBinding.txtNoCliente.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        mainBinding.btnContinuarArriba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),PinImgActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        mainBinding.txtNoCliente.setPaintFlags(mainBinding.txtNoCliente.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         mainBinding.etIngresarUsuario.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(charSequence.toString().equals("")){
+                if (charSequence.toString().equals("")) {
                     mainBinding.btnContinuar.setBackgroundColor(Color.GRAY);
                     return;
-                }else{
+                } else {
                     mainBinding.btnContinuar.setBackgroundColor(Color.YELLOW);
                     return;
                 }
@@ -44,18 +78,76 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(editable.toString().equals("")){
+                if (editable.toString().equals("")) {
                     mainBinding.btnContinuar.setBackgroundColor(Color.GRAY);
                     return;
-                }else{
+                } else {
                     mainBinding.btnContinuar.setBackgroundColor(Color.YELLOW);
                     return;
                 }
             }
         });
+
+        //login
     }
-    public void goToPinImg(View view){
-        Intent intent = new Intent(this,PinImgActivity.class);
-        startActivity(intent);
+
+    public void userValidation(View view) {
+
+        username = mainBinding.etIngresarUsuario.getText().toString();
+        //final TextView textView = (TextView) findViewById(R.id.text);
+        // ...
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.0.3:8080/appbancolombia/auth/pruebaLogn.php";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("VOLLEY",response);
+                        // Display the first 500 characters of the response string.
+                        //textView.setText("Response is: "+ response.substring(0,500));
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String  image = jsonObject.optString("image","null");
+                            String status = jsonObject.optString("status","ok");
+
+                            if(status.equals("bad")){
+                                Toast.makeText(getApplicationContext(),
+                                        "Failure Login: "+status, Toast.LENGTH_SHORT).show();
+                            }else if(status.equals("ok")){
+                                Toast.makeText(getApplicationContext(),
+                                        "Success Login: "+image, Toast.LENGTH_SHORT).show();
+                               Intent intent = new Intent(getApplicationContext(),PinImgActivity.class);
+                                intent.putExtra("image",image);
+                                intent.putExtra("username",username);
+                                startActivity(intent);
+                            }
+                        } catch (JSONException e) {
+                            //e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error JSON"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+                Toast.makeText(getApplicationContext(), "error"+error.getMessage().toString(
+                ), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> data = new HashMap<>();
+                data.put("username",username);
+                return data;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
--- app/src/main/java/com/example/bancolombiaapp/PinImgActivity.java
+++ app/src/main/java/com/example/bancolombiaapp/PinImgActivity.java
@@ -7,21 +7,56 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.bancolombiaapp.databinding.ActivityPinImgBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PinImgActivity extends AppCompatActivity {
 
     private ActivityPinImgBinding pinImgBinding;
-
+    private int pin;
+    private String username;
+    private String user;
+    private String image;
+    ImageView imageView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         pinImgBinding = ActivityPinImgBinding.inflate(getLayoutInflater());
         View view = pinImgBinding.getRoot();
         setContentView(view);
+        username = getIntent().getStringExtra("username");
+        image = getIntent().getStringExtra("image");
 
+        imageView = findViewById(R.id.img);
+        String url = image;
+        Glide.with(this)
+                .load(url)
+                .placeholder(R.drawable.imgbanco)
+                .error(R.drawable.ic_launcher_background)
+                .circleCrop()
+                .into(imageView);
+
+
+        //Cambio de color en boton continuar
         pinImgBinding.pinView.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -50,9 +85,72 @@ public class PinImgActivity extends AppCompatActivity {
                 }
             }
         });
+        //volver a la activicty de usuario
+        pinImgBinding.btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void goToMainActivity(View view){
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+
+
+    //Recibiendo las respuestas JSON
+    public void pinValidate(View view){
+        pin = Integer.parseInt(pinImgBinding.pinView.getText().toString());
+        //final TextView textView = (TextView) findViewById(R.id.text);
+        // ...
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.0.3:8080/appbancolombia/login/pruebaLogin.php";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("VOLLEY",response);
+                        // Display the first 500 characters of the response string.
+                        //textView.setText("Response is: "+ response.substring(0,500));
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String  account_number = jsonObject.optString("account_number","null");
+                            String status = jsonObject.optString("status","ok");
+
+                            if(status.equals("bad")){
+                                Toast.makeText(getApplicationContext(),
+                                        "Verify Your PIN: "+status, Toast.LENGTH_SHORT).show();
+                            }else if(status.equals("ok")){
+                                Toast.makeText(getApplicationContext(),
+                                        "Success PIN: "+pin+","+account_number, Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            //e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error JSON"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+                Toast.makeText(getApplicationContext(), "error"+error.getMessage().toString(
+                ), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> data = new HashMap<>();
+                data.put("username",username);
+                data.put("pin",Integer.toString(pin));
+                return data;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
+
 }
